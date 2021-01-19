0d8ef451fe6e0e24bf2a326ff0a0dedb5f8bd239
Tyler Sinks <tyler.sinks@gmail.com>
2021-01-19 18:27:22 -0500

    add counting func, and UI display results
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
+function counter(countUpTo,countingBy){
+  const countingArray=[];
+  for (let count=0;count<=countUpTo;count+=countingBy){
+    countingArray.push(count);
+  }
+  return countingArray;
+}
+
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     const maxCount = parseInt($("input#max-count").val());
     const countBy = parseInt($("input#multiple").val());
-
-    
+    let results = counter(maxCount,countBy);
+    let htmlText = '';
+    results = results.forEach(function(num) {
+      htmlText += "<li> "+ num+"</li>";
+    })
+    $("ul").prepend(htmlText);
 
   })
 })
