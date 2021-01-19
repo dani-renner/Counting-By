bc31e77aea8af8ed69dc1a6b9e06137ac8bdee51
Tyler Sinks <tyler.sinks@gmail.com>
2021-01-19 17:54:35 -0500

    add basic file structure, text entry fields
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("document").ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const maxCount = parseInt($("input#max-count").val());
+    const countBy = parseInt($("input#multiple").val());
+
+    
+
+  })
+})
